57ae8fb14d93c04d1f6ac2f333e7fa3e8ca23916
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-12 12:02:11 +0200

    Add user permission for methods list & create

--- apps/users/views.py
+++ apps/users/views.py
@@ -15,9 +15,17 @@ class UserViewSet(ListModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes_by_action = {
-        'create': [AllowAny],
-        'list': [IsAuthenticated],
+        'create': (AllowAny,),
+        'list': (IsAuthenticated,),
     }
+
+    def get_permissions(self):
+        try:
+            # return permission_classes depending on `action`
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
 
     @action(
         methods=['POST'],
