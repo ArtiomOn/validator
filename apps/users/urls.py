56e039ab151449317ffe9c6c80ec3191274907ec
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-12 13:50:31 +0200

    Add trailing_slash to False

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 
 from apps.users.views import UserViewSet
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
+router = routers.DefaultRouter(trailing_slash=False)
+router.register('', UserViewSet, basename='user')
 
 urlpatterns = router.urls
