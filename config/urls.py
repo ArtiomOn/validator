7144c0a19873d9d6d8f951301bcebd982b701b74
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-12 13:50:17 +0200

    Redact

--- config/urls.py
+++ config/urls.py
@@ -20,10 +20,10 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0),  # noqa
          name='schema-swagger-ui'),
     path('users/', include('apps.users.urls')),
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('jwt/', include([
-        path('token/', TokenObtainPairView.as_view(), name='token_obtain-pair'),
-        path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
-        path('token/verify/', TokenVerifyView.as_view(), name='token-verify'),
+        path('token', TokenObtainPairView.as_view(), name='token_obtain-pair'),
+        path('token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
+        path('token/verify', TokenVerifyView.as_view(), name='token-verify'),
     ])),
 ]
