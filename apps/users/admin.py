a47b882fd31ef5195f33d009e81feee61a09c2c0
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-14 14:31:48 +0200

    Add user in admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+from apps.users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_active', 'is_staff', 'is_superuser', 'updated_at', 'created_at')
+    list_filter = ('is_active', 'is_staff', 'updated_at', 'created_at')
+    search_fields = ('email', 'updated_at', 'created_at')
+    ordering = ('email', 'updated_at', 'created_at')
