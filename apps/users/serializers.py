4c34573050df1b7e6b9389267986d91c1622725e
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-14 14:33:29 +0200

    Update user serializer for read method

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,11 +1,22 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
 User = get_user_model()
 
+__all__ = (
+    'UserRegisterSerializer',
+    'UserListSerializer',
+)
 
-class UserSerializer(serializers.ModelSerializer):
+
+class UserRegisterSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'password')
+
+
+class UserListSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'is_staff', 'is_active')
