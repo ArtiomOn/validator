c0662be527444a925ca0241daf9728414b0fda15
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-14 14:34:25 +0200

    Add email & imei views

--- apps/methods/views.py
+++ apps/methods/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from apps.methods.models import Email, IMEI
+from apps.methods.serializers import EmailSerializer, IMEISerializer
+
+
+class EmailViewSet(viewsets.ModelViewSet):
+    queryset = Email.objects.all()
+    serializer_class = EmailSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class IMEIViewSet(viewsets.ModelViewSet):
+    queryset = IMEI.objects.all()
+    serializer_class = IMEISerializer
+    permission_classes = (IsAuthenticated,)
