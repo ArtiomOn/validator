a98e392d4cef1c26756599e84c56754211ee909c
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-19 11:29:43 +0200

    Add IMEI & Email validation

--- apps/methods/serializers.py
+++ apps/methods/serializers.py
@@ -1,14 +1,36 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
+
 from apps.methods.models import Email, IMEI
+from apps.methods.validators.validators import EmailValidator, IMEIValidator
 
 
 class EmailSerializer(ModelSerializer):
+    email = CharField(
+        validators=[
+            EmailValidator()
+        ],
+        required=True,
+    )
+
     class Meta:
         model = Email
         fields = '__all__'
+        extra_kwargs = {
+            'is_valid': {'read_only': True},
+        }
 
 
 class IMEISerializer(ModelSerializer):
+    imei = CharField(
+        validators=[
+            IMEIValidator()
+        ],
+        required=True,
+    )
+
     class Meta:
         model = IMEI
         fields = '__all__'
+        extra_kwargs = {
+            'is_valid': {'read_only': True},
+        }
