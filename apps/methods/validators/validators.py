5af02a0b1f9670ad44729b0a621c63d662655048
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-19 11:30:02 +0200

    Redact validation

--- apps/methods/validators/validators.py
+++ apps/methods/validators/validators.py
@@ -1,15 +1,16 @@
-import re
 import math
+import re
 from ipaddress import IPv4Address, IPv6Address
 
+from rest_framework.exceptions import ValidationError
 from validate_email import validate_email
 
 
-class Validator:
-    @staticmethod
-    def email_validator(email):
+class EmailValidator:
+
+    def __call__(self, email: str):
         if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            raise ValueError("Email is invalid")
+            raise ValidationError("Email is invalid")
         is_valid = validate_email(
             email_address=email,
             check_format=True,
@@ -21,21 +22,28 @@ class Validator:
             smtp_debug=False,
             address_types=frozenset([IPv4Address, IPv6Address])
         )
-        return bool(is_valid)
+        if not is_valid:
+            raise ValidationError("Email is invalid")
 
-    @classmethod
-    def imei_validator(cls, imei: str):
+
+class IMEIValidator:
+
+    def __call__(self, imei: str):
+        # Check IMEI length (15 digits)
+        if not re.match(r"^\d{15}$", imei):
+            raise ValidationError("IMEI is invalid")
         check_imei, validation_digit, = imei[:-1], imei[-1]
         # Multiply every second element with 2
         check_imei = [int(num) * 2 if idx % 2 != 0 else int(num) for idx, num in enumerate(check_imei)]
         # Separate imei into single digits and sum them
         imei_sum = sum(list(map(int, ''.join(str(e) for e in check_imei))))
         # Round it up to the nearest multiple of ten
-        rounded_imei = cls.round_up_multiple(imei_sum)
+        rounded_imei = self.round_up_multiple(imei_sum)
         # Subtract original number from the rounded-up number
         difference = rounded_imei - imei_sum
         # If two numbers match - imei is valid
-        return True if difference == int(validation_digit) else False
+        if not difference == int(validation_digit):
+            raise ValidationError("IMEI is invalid")
 
     @staticmethod
     def round_up_multiple(number):
