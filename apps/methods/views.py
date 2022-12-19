df7706148a0cf77e3da3ea226feb8dbd1b3bc541
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-19 11:30:50 +0200

    Add permissions for IMEI and Email viewset

--- apps/methods/views.py
+++ apps/methods/views.py
@@ -1,17 +1,43 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+from apps.common.views import BasicModelViewSet
 from apps.methods.models import Email, IMEI
 from apps.methods.serializers import EmailSerializer, IMEISerializer
 
 
-class EmailViewSet(viewsets.ModelViewSet):
+# noinspection DuplicatedCode
+class EmailViewSet(BasicModelViewSet):
     queryset = Email.objects.all()
     serializer_class = EmailSerializer
     permission_classes = (IsAuthenticated,)
+    permission_by_action = {
+        'list': [IsAuthenticated],
+        'create': [IsAuthenticated],
+        'delete': [IsAdminUser],
+        'update': [IsAdminUser],
+    }
+
+    def perform_create(self, serializer):
+        # If the email is valid, then the is_valid field will be set to True
+        serializer.save(
+            is_valid=True
+        )
 
 
-class IMEIViewSet(viewsets.ModelViewSet):
+# noinspection DuplicatedCode
+class IMEIViewSet(BasicModelViewSet):
     queryset = IMEI.objects.all()
     serializer_class = IMEISerializer
     permission_classes = (IsAuthenticated,)
+    permission_by_action = {
+        'list': [IsAuthenticated],
+        'create': [IsAuthenticated],
+        'delete': [IsAdminUser],
+        'update': [IsAdminUser],
+    }
+
+    def perform_create(self, serializer):
+        # If the imei is valid, then the is_valid field will be set to True
+        serializer.save(
+            is_valid=True
+        )
