6644d3c5bb69113a65119bca7dc3e6d86d51d6b4
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-23 15:29:03 +0200

    Add imei generator

--- apps/methods/views.py
+++ apps/methods/views.py
@@ -1,9 +1,9 @@
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
 
 from apps.common.views import ExtendedRetrieveUpdateDestroyAPIView
+from apps.methods.generators.imei_generator import ImeiGenerator
 from apps.methods.models import Email, IMEI
 from apps.methods.serializers import EmailSerializer, IMEISerializer
 
@@ -12,10 +12,8 @@ from apps.methods.serializers import EmailSerializer, IMEISerializer
 class EmailViewSet(ExtendedRetrieveUpdateDestroyAPIView):
     queryset = Email.objects.all()
     serializer_class = EmailSerializer
-    permission_classes = (IsAuthenticated,)
     permission_by_action = {
-        'user_email': [IsAuthenticated],
-        'email_check': [IsAuthenticated],
+        'default': [IsAuthenticated],
         'delete': [IsAdminUser],
         'update': [IsAdminUser],
     }
@@ -24,14 +22,14 @@ class EmailViewSet(ExtendedRetrieveUpdateDestroyAPIView):
     def email_check(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # If the email is valid, then the is_valid field will be set to True
-        serializer.save(
-            is_valid=True,
-            user=request.user,
-        )
-        return Response(serializer.data, status=HTTP_201_CREATED)
+        if request.user.is_authenticated:
+            serializer.save(
+                is_valid=True,
+                user=request.user,
+            )
+        return Response(serializer.data)
 
-    @email_check.mapping.get
+    @action(detail=False, methods=['get'], url_path='user_email', url_name='user_email')
     def user_email(self, request, *args, **kwargs):
         queryset = self.queryset.filter(user=request.user)
         serializer = self.get_serializer(queryset, many=True)
@@ -41,11 +39,12 @@ class EmailViewSet(ExtendedRetrieveUpdateDestroyAPIView):
 # noinspection DuplicatedCode
 class IMEIViewSet(ExtendedRetrieveUpdateDestroyAPIView):
     queryset = IMEI.objects.all()
-    serializer_class = IMEISerializer
-    permission_classes = (IsAuthenticated,)
+    serializers_by_action = {
+        "default": IMEISerializer,
+    }
     permission_by_action = {
-        'user_imei': [IsAuthenticated],
-        'imei_check': [IsAuthenticated],
+        'default': [IsAuthenticated],
+        'generate_imei': [AllowAny],
         'delete': [IsAdminUser],
         'update': [IsAdminUser],
     }
@@ -54,14 +53,26 @@ class IMEIViewSet(ExtendedRetrieveUpdateDestroyAPIView):
     def imei_check(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # If the imei is valid, then the is_valid field will be set to True
-        serializer.save(
-            is_valid=True,
-            user=request.user
-        )
-        return Response(serializer.data, status=HTTP_201_CREATED)
+        if request.user.is_authenticated:
+            serializer.save(
+                is_valid=True,
+                user=request.user
+            )
+        return Response(serializer.data)
 
-    @imei_check.mapping.get
+    @action(detail=False, methods=['post'], url_path='generate-imei', url_name='generate-imei')
+    def generate_imei(self, request, *args, **kwargs):
+        imei = ImeiGenerator().generate()
+        serializer = self.get_serializer(data={'imei': imei})
+        serializer.is_valid(raise_exception=True)
+        if request.user.is_authenticated:
+            serializer.save(
+                is_valid=True,
+                user=request.user
+            )
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='user_imei', url_name='user_imei')
     def user_imei(self, request, *args, **kwargs):
         queryset = self.queryset.filter(user=request.user)
         serializer = self.get_serializer(queryset, many=True)
