c52e0353e3b5c2dba3e520a466c72186e9167e15
ArtiomOn <artiom.carter.03@mail.ru>
2022-12-23 16:08:08 +0200

    Refactor

--- apps/methods/tests.py
+++ apps/methods/tests.py
@@ -1,8 +1,8 @@
-from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework import status
 from faker import Faker
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 fake = Faker()
