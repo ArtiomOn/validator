c2f8f01d2518e11ceea3e9ba9bef4b2e91712527
ArtiomOn <artiom.carter.03@mail.ru>
2023-01-05 17:57:00 +0200

    Fix email validator

--- apps/validations/validators/validators.py
+++ apps/validations/validators/validators.py
@@ -3,7 +3,8 @@ import re
 from ipaddress import IPv4Address, IPv6Address
 
 from rest_framework.exceptions import ValidationError
-from validate_email import validate_email
+from validate_email import validate_email_or_fail
+from validate_email.exceptions import DNSTimeoutError
 
 
 class EmailValidator:
@@ -11,17 +12,20 @@ class EmailValidator:
     def __call__(self, email: str):
         if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
             raise ValidationError("Email is invalid")
-        is_valid = validate_email(
-            email_address=email,
-            check_format=True,
-            check_blacklist=True,
-            check_smtp=True,
-            smtp_timeout=10,
-            smtp_skip_tls=False,
-            smtp_tls_context=None,
-            smtp_debug=False,
-            address_types=frozenset([IPv4Address, IPv6Address])
-        )
+        try:
+            is_valid = validate_email_or_fail(
+                email_address=email,
+                check_format=True,
+                check_blacklist=True,
+                check_smtp=True,
+                smtp_timeout=10,
+                smtp_skip_tls=False,
+                smtp_tls_context=None,
+                smtp_debug=False,
+                address_types=frozenset([IPv4Address, IPv6Address])
+            )
+        except DNSTimeoutError:
+            raise ValidationError("Service is unavailable")
         if not is_valid:
             raise ValidationError("Email is invalid")
 
