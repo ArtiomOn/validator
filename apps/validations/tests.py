d911e18698eaf9f917a7966ce660e842d0627643
oncea.artiom <artiom.carter.03@mail.ru>
2023-01-09 11:03:17 +0200

    Redact

--- apps/validations/tests.py
+++ apps/validations/tests.py
@@ -23,14 +23,10 @@ class MethodsTestCase(APITestCase):
         )
 
     def test_email_check(self):
-        data = {
-            "email": "test@test.com",
-        }
+        data = {"email": "test@test.com"}
         response = self.client.post("/validations/email/email_check", data=data, **auth(user=self.user))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        data = {
-            "email": "artiom@gmail.com",
-        }
+        data = {"email": "artiom@gmail.com"}
         # if email check service is unavailable - it will return 400 STATUS CODE
         response = self.client.post("/validations/email/email_check", data=data, **auth(user=self.user))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -39,14 +35,10 @@ class MethodsTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_imei_check(self):
-        data = {
-            "imei": "123456789012345",
-        }
+        data = {"imei": "123456789012345"}
         response = self.client.post("/validations/imei/imei_check", data=data, **auth(user=self.user))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        data = {
-            "imei": "511557981902548",
-        }
+        data = {"imei": "511557981902548"}
         response = self.client.post("/validations/imei/imei_check", data=data, **auth(user=self.user))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response = self.client.get("/validations/imei/user_imei", **auth(user=self.user))
